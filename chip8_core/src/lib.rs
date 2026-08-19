57c5028de498783a7e3f738e2cdc18c18012f163
AbdelrhmanAtta <abdelrhmatta@gmail.com>
2026-08-19 14:04:27 +0300

    [Fix] Swapped width with height

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,5 +1,5 @@
-pub const SCREEN_HEIGHT: usize = 64;
-pub const SCREEN_WIDTH: usize = 32;
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
 
 const RAM_SIZE: usize = 4096;
 const REGISTERS_NUMBER: usize = 16;
