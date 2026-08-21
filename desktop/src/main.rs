a9384a8ea7c408c610402402deede766e0d04899
Nancy Ashraf <nancyashraf834@gmail.com>
2026-08-21 18:56:39 +0300

    frontend done

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,8 +1,11 @@
 use chip8_core::*;
-use std::env;
-use sdl2::event::Event;
+
 use std::fs::File;
 use std::io::Read;
+use std::env;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
@@ -11,6 +14,7 @@ use sdl2::video::Window;
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRAME: usize = 10;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -35,7 +39,7 @@ fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut chip8 = Emulator::new();
+    let mut chip8 = Emu::new();
 
     let mut rom = File::open(&args[1]).expect("Unable to open file");
     let mut buffer = Vec::new();
@@ -46,20 +50,32 @@ fn main() {
     'gameloop: loop {
         for evt in event_pump.poll_iter() {
             match evt {
-                Event::Quit{..} => {
+                Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..}=> {
                     break 'gameloop;
+                },
+                Event::KeyDown{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, true);
+                    }
+                },
+                Event::KeyUp{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, false);
+                    }
                 },
                 _ => ()
             }
         }
 
-        chip8.tick();
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+        chip8.tick_timers();
         draw_screen(&chip8, &mut canvas);
     }
-
 }
 
-fn draw_screen(emu: &Emulator, canvas: &mut Canvas<Window>) {
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
     // Clear canvas as black
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
@@ -79,4 +95,39 @@ fn draw_screen(emu: &Emulator, canvas: &mut Canvas<Window>) {
         }
     }
     canvas.present();
+}
+
+/*
+    Keyboard                    Chip-8
+    +---+---+---+---+           +---+---+---+---+
+    | 1 | 2 | 3 | 4 |           | 1 | 2 | 3 | C |
+    +---+---+---+---+           +---+---+---+---+
+    | Q | W | E | R |           | 4 | 5 | 6 | D |
+    +---+---+---+---+     =>    +---+---+---+---+
+    | A | S | D | F |           | 7 | 8 | 9 | E |
+    +---+---+---+---+           +---+---+---+---+
+    | Z | X | C | V |           | A | 0 | B | F |
+    +---+---+---+---+           +---+---+---+---+
+*/
+
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 =>    Some(0x1),
+        Keycode::Num2 =>    Some(0x2),
+        Keycode::Num3 =>    Some(0x3),
+        Keycode::Num4 =>    Some(0xC),
+        Keycode::Q =>       Some(0x4),
+        Keycode::W =>       Some(0x5),
+        Keycode::E =>       Some(0x6),
+        Keycode::R =>       Some(0xD),
+        Keycode::A =>       Some(0x7),
+        Keycode::S =>       Some(0x8),
+        Keycode::D =>       Some(0x9),
+        Keycode::F =>       Some(0xE),
+        Keycode::Z =>       Some(0xA),
+        Keycode::X =>       Some(0x0),
+        Keycode::C =>       Some(0xB),
+        Keycode::V =>       Some(0xF),
+        _ =>                None,
+    }
 }
