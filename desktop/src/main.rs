edde7c3c8502238c9dd231c939075bd99b13831d
Nancy Ashraf <nancyashraf834@gmail.com>
2026-08-21 18:57:07 +0300

    frontend done

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -39,7 +39,7 @@ fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut chip8 = Emu::new();
+    let mut chip8 = Emulator::new();
 
     let mut rom = File::open(&args[1]).expect("Unable to open file");
     let mut buffer = Vec::new();
